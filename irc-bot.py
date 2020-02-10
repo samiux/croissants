66ac6f2e8f8d6a7ff35c2b245d4b29cf6f7f94e4
Samiux <infosecninjas@gmail.com>
2020-02-10 16:25:42 +0800

    Add files via upload

--- irc-bot.py
+++ irc-bot.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 # Description : IRC Bot for Croissants Clients
 # Purpose     : To count for the total number of Croissants users only.
 #               It can do nothing other than idling at the IRC channel.
 # Source      : https://www.infragistics.com/community/blogs/torrey-betts/archive/2016/04/04/create-an-irc-bot-using-python-2.aspx
 #               http://chamilad.github.io/blog/2015/11/26/timing-out-of-long-running-methods-in-python/
 # Modified by : Samiux (https://www.infosec-ninjas.com)
-# Date        : Oct 08, 2017 GMT+8
+# Date        : Feb 10, 2020 GMT+8
 
 import random
 import socket
@@ -13,23 +15,13 @@ import time
 import ssl
 import os
 import signal
-
-reload(sys)
-sys.setdefaultencoding('utf8')
+import subprocess
 
 server = "chat.freenode.net"
 channel = "#croissants"
 botnick = "ninjas-" + str(random.randint(1, 100000000))
 port = 7070
-wait = 60
-
-global administrators
-global nick
-global completed_ubuntu
-global completed_rules
-global completed_auto
-global completed_suricata
-global completed_delete
+wait = 10
 
 administrators = ( "samiux" )
 nick = ""
@@ -37,126 +29,81 @@ completed_ubuntu = None
 completed_rules = None
 completed_auto = None
 completed_suricata = None
-completed_delete = None
-
-# timeout handler
-def timeout_handler(num, stack):
-  print("Received SIGALRM [%s]" % num)
-  raise Exception("FUBAR")
 
 # connect routine function
 def connect():
+
   global administrators
   global nick
   global completed_ubuntu
   global completed_rules
   global completed_auto
   global completed_suricata
-  global completed_delete
 
-  last_ping = time.time()
-  threshold = 5 * 60
+  ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  irc_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  irc = ssl.wrap_socket(irc_s)
-  print "\nConnecting to:" + server
+  irc = ssl.wrap_socket(ircsock)
+  irc.settimeout(240)
+  print ("\nConnecting to:" + server)
   irc.connect((server, port))
-  irc.send("USER " + botnick + " " + botnick + " " + botnick + " :This is an idle bot\n")
-  irc.send("NICK " + botnick + "\n")
-  irc.send("JOIN " + channel + "\n")
+
+  irc.send(bytes(str("USER " + botnick + " " + botnick + " " + botnick + " :This is an idle bot\n"), "UTF-8"))
+  irc.send(bytes(str("NICK " + botnick + "\n"), "UTF-8"))
+  irc.send(bytes(str("JOIN " + channel + "\n"), "UTF-8"))
 
   try:
     while True:
-      signal.signal(signal.SIGALRM, timeout_handler)
-      signal.alarm(threshold)
-
-      if completed_ubuntu == False:
-        irc.send( "PRIVMSG " + channel + " :" + "Ubuntu Update done even timeout!\n")
-        completed_ubuntu = None
-
-      if completed_rules == False:
-        irc.send( "PRIVMSG " + channel + " :" + "Rules Update done even timeout!\n" )
-        completed_rules = None
-
-      if completed_auto == False:
-        irc.send( "PRIVMSG " + channel + " :" + "Auto Update done even timeout!\n" )
-        completed_auto = None
-
-      if completed_delete == False:
-        irc.send( "PRIVMSG " + channel + " :" + "Waste delete done even timeout!\n" )
-        completed_suricata = None
-
-      text = irc.recv(4096)
-      if len(text) > 0:
-        print text
-      else:
-        break
 
       # get the nickname of command issued
+      text = irc.recv(8192).decode("UTF-8")
+      text = text.strip('\n\r')
+      print (text)
+
       nick = text.split(":")[1]
       nick = nick.split("!")[0]
 
       # re-join to the channel when kick after preset seconds
       if text.find( "KICK" ) != -1:
         time.sleep(wait)
-        irc.send("JOIN " + channel + "\n")
+        irc.send(bytes(str("JOIN " + channel), "UTF-8"))
 
-      # to keep client alive
+      # to check version number of suricata
+      if text.find( "CHECK_VERSION" ) != -1:
+        msg = subprocess.getoutput("sudo /usr/bin/suricata -V").strip('\n\r')
+        irc.send(bytes(str("PRIVMSG " + channel + " :" + msg +"\n"), "UTF-8"))
+
+      # to ping and waiting for pong
       if text.find( "PING" ) != -1:
-        irc.send("PONG " + text.split()[1] + "\n")
-        last_ping = time.time()
+        irc.send(bytes(str("PONG " + text.split()[1] + "\n"), "UTF-8"))
 
-      # custom commands
-      if text.find( ":!ubuntu_update" ) != -1:
+      # to update ubuntu
+      if text.find( "UBUNTU_UPDATE" ) != -1:
         if nick in administrators:
-          completed_ubuntu = False
-          os.system("sudo /usr/bin/update_ubuntu")
-          irc.send( "PRIVMSG " + channel + " :" + "Ubuntu Update done!\n")
-          completed_ubuntu = True
+          subprocess.getoutput("sudo /usr/bin/update_ubuntu")
+          irc.send(bytes(str("PRIVMSG " + channel + " :" + "Ubuntu Update is done!\n"), "UTF-8"))
 
-      if text.find( ":!rules_update" ) != -1:
+      # to update suricata rules
+      if text.find( "RULES_UPDATE" ) != -1:
         if nick in administrators:
-          completed_rules = False
-          os.system("sudo /usr/bin/nsm_rules_update")
-          irc.send( "PRIVMSG " + channel + " :" + "Rules Update done!\n" )
-          completed_rules = True
+          subprocess.getoutput("sudo /usr/bin/nsm_rules_update")
+          irc.send(bytes(str("PRIVMSG " + channel + " :" + "Rules Update is done!\n"), "UTF-8"))
 
-      if text.find( ":!auto_update" ) != -1:
+      # to conduct auto update/upgrade
+      if text.find( "AUTO_UPDATE" ) != -1:
         if nick in administrators:
-          completed_auto = False
-          os.system("sudo /etc/croissants/conf.d/auto_update")
-          irc.send( "PRIVMSG " + channel + " :" + "Auto Update done!\n" )
-          completed_auto = True
+          subprocess.getoutput("sudo /etc/croissants/conf.d/auto_update")
+          irc.send(bytes(str("PRIVMSG " + channel + " :" + "Auto Update is done!\n"), "UTF-8"))
 
-      if text.find( ":!suricata_restart" ) != -1:
+      # to restart suricata
+      if text.find( "SURICATA_RESTART" ) != -1:
         if nick in administrators:
-          completed_suricata = False
-          os.system("sudo systemctl restart suricata")
-          irc.send( "PRIVMSG " + channel + " :" + "Suricata Restart done!\n" )
-          completed_suricata = True
-
-      if text.find( ":!waste_delete" ) != -1:
-        if nick in administrators:
-          completed_delete = False
-          os.system("sudo /etc/croissants/conf.d/delete_waste")
-          irc.send( "PRIVMSG " + channel + " :" + "Wastes delete done!\n" )
-          completed_delete = True
-
-  except Exception as ex:
-    if "FUBAR" in ex:
-      print("Timeout already!  Re-connecting ....")
-      pass
-    else:
-      print ex
-      sys.exit()
+          subprocess.getoutput("sudo systemctl restart suricata")
+          irc.send(bytes(str("PRIVMSG " + channel + " :" + "Suricata Restart is done!\n"), "UTF-8"))
 
   except KeyboardInterrupt:
-    irc.send("QUIT :I have to go for now!\n")
-    print "\n"
+    irc.send(bytes(str("QUIT :I have to go for now!\n"), "UTF-8"))
+    print ("\n")
     sys.exit()
-
-  finally:
-    signal.alarm(0)
 
 # main routine
 if not os.geteuid()==0:
